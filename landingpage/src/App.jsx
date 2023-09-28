2ac2484d51efdc07033a02f5d583091a203d44c9
Mahmoud Fouad Ali <mfapolice@gmail.com>
2023-09-29 00:14:14 +0300

    first commit

---
+++ landingpage/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import main from "./layout/home/main";
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
---
+++ landingpage/src/layout/home/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const main = () => {
+  return (
+    <nav>
+    <div>
+      <img src="./assets/home/desktop/Combined Shape.png" alt="" />
+   </div>
+    <ul>
+      
+    </ul>
+</nav>
+  )
+}
+
+export default main
