dd89ccb1cb3977539807e2985d3137f72bc11dfb
Mahmoud Fouad Ali <mfapolice@gmail.com>
2023-09-30 04:06:30 +0300

    update

--- landingpage/src/layout/home/Home.jsx
+++ landingpage/src/layout/home/Home.jsx
@@ -4,6 +4,9 @@ import granEspresso from "../../assets/home/desktop/image-gran-espresso.png";
 import planalto from "../../assets/home/desktop/image-planalto.png";
 import Piccollo from "../../assets/home/desktop/image-Piccollo.png";
 import Danche from "../../assets/home/desktop/image-danche.png";
+import Coffee from "../../assets/home/desktop/icon-coffee-bean.svg";
+import benefits from "../../assets/home/desktop/icon-gift.svg";
+import shipping from "../../assets/home/desktop/icon-truck.svg";
 
 const main = () => {
   return (
@@ -11,8 +14,6 @@ const main = () => {
       <nav>
         <div className="logo">
           <img src={logoTitleSV} alt="aaa" />
-          {/* <img src={logoTitleSVG} alt="aaa" /> */}
-          {/* <p>coffeeroasters</p> */}
         </div>
         <ul className="nav_list">
           <li>home</li>
@@ -68,6 +69,76 @@ const main = () => {
           </div>
         </div>
       </div>
+
+      {/* Why choose us? */}
+      <section className="why_choose_us">
+        <h2>Why choose us?</h2>
+        <p>
+          A large part of our role is choosing which particular coffees will be
+          featured in our range. This means working closely with the best coffee
+          growers to give you a more impactful experience on every level.
+        </p>
+
+        <div className="why_choose_us_cards">
+          <div className="why_choose_us_card">
+            <img src={Coffee} alt="Coffee" />
+            <h3>Best quality</h3>
+            <p>
+              Discover an endless variety of the world’s best artisan coffee
+              from each of our roasters.
+            </p>
+          </div>
+          <div className="why_choose_us_card">
+            <img src={benefits} alt="benefits" />
+            <h3>Exclusive benefits</h3>
+            <p>
+              Special offers and swag when you subscribe, including 30% off your
+              first shipment.
+            </p>
+          </div>
+          <div className="why_choose_us_card">
+            <img src={shipping} alt="Coffee" />
+            <h3>Free shipping</h3>
+            <p>
+              We cover the cost and coffee is delivered fast. Peak freshness:
+              guaranteed.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="how_works">
+        <h3>How it works</h3>
+        <div className="steps">
+          <div className="steps_card">
+            <div className="step_number">01</div>
+            <h2 className="step_title">Pick your coffee</h2>
+            <p>
+              Select from our evolving range of artisan coffees. Our beans are
+              ethically sourced and we pay fair prices for them. There are new
+              coffees in all profiles every month for you to try out.
+            </p>
+          </div>
+          <div className="steps_card">
+            <div className="step_number">02</div>
+            <h2 className="step_title">Choose the frequency</h2>
+            <p>
+              Customize your order frequency, quantity, even your roast style
+              and grind type. Pause, skip or cancel your subscription with no
+              commitment through our online portal.
+            </p>
+          </div>
+          <div className="steps_card">
+            <div className="step_number">03</div>
+            <h2 className="step_title">Receive and enjoy!</h2>
+            <p>
+              We ship your package within 48 hours, freshly roasted. Sit back
+              and enjoy award-winning world-class coffees curated to provide a
+              distinct tasting experience.
+            </p>
+          </div>
+        </div>
+        <button>Create Your Plan</button>
+      </section>
     </>
   );
 };
